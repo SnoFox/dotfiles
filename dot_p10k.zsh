9232f1656c72e08469caa22400d35e778869eadd
SnoFox <snofox@snofox.net>
2023-03-20 13:03:45 -0700

    Make zellij "default shell"

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -49,10 +49,10 @@
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
-    #direnv                  # direnv status (https://direnv.net/)
+    # direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-    #anaconda                # conda environment (https://conda.io/)
+    # anaconda                # conda environment (https://conda.io/)
     pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
     nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
@@ -68,14 +68,14 @@
     # package               # name@version from package.json (https://docs.npmjs.com/files/package.json)
     rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
     rvm                     # ruby version from rvm (https://rvm.io)
-    #fvm                     # flutter version management (https://github.com/leoafarias/fvm)
-    #luaenv                  # lua version from luaenv (https://github.com/cehoffman/luaenv)
-    #jenv                    # java version from jenv (https://github.com/jenv/jenv)
-    #plenv                   # perl version from plenv (https://github.com/tokuhirom/plenv)
-    #perlbrew                # perl version from perlbrew (https://github.com/gugod/App-perlbrew)
-    #phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
-    #scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
-    #haskell_stack           # haskell version from stack (https://haskellstack.org/)
+    # fvm                     # flutter version management (https://github.com/leoafarias/fvm)
+    # luaenv                  # lua version from luaenv (https://github.com/cehoffman/luaenv)
+    # jenv                    # java version from jenv (https://github.com/jenv/jenv)
+    # plenv                   # perl version from plenv (https://github.com/tokuhirom/plenv)
+    # perlbrew                # perl version from perlbrew (https://github.com/gugod/App-perlbrew)
+    # phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
+    # scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
+    # haskell_stack           # haskell version from stack (https://haskellstack.org/)
     kubecontext             # current kubernetes context (https://kubernetes.io/)
     terraform               # terraform workspace (https://www.terraform.io)
     # terraform_version     # terraform version (https://www.terraform.io)
@@ -86,23 +86,22 @@
     google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
     toolbox                 # toolbox name (https://github.com/containers/toolbox)
     context                 # user@hostname
-    #nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
-    #ranger                  # ranger shell (https://github.com/ranger/ranger)
-    #nnn                     # nnn shell (https://github.com/jarun/nnn)
-    #lf                      # lf shell (https://github.com/gokcehan/lf)
-    #xplr                    # xplr shell (https://github.com/sayanarijit/xplr)
-    #vim_shell               # vim shell indicator (:sh)
-    #midnight_commander      # midnight commander shell (https://midnight-commander.org/)
-    #nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
+    # ranger                  # ranger shell (https://github.com/ranger/ranger)
+    # nnn                     # nnn shell (https://github.com/jarun/nnn)
+    # lf                      # lf shell (https://github.com/gokcehan/lf)
+    # xplr                    # xplr shell (https://github.com/sayanarijit/xplr)
+    # vim_shell               # vim shell indicator (:sh)
+    # midnight_commander      # midnight commander shell (https://midnight-commander.org/)
+    # nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # vi_mode               # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
     # ram                   # free RAM
     # swap                  # used swap
-    #todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
-    #timewarrior             # timewarrior tracking status (https://timewarrior.net/)
-    #taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
+    # todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
+    # timewarrior             # timewarrior tracking status (https://timewarrior.net/)
+    # taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     # cpu_arch              # CPU architecture
     time                    # current time
     # =========================[ Line #2 ]=========================
@@ -110,7 +109,7 @@
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    # battery               # internal battery
+    battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
   )
